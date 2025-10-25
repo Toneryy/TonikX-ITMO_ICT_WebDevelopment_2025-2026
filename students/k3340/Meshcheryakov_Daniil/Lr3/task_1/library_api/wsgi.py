1548c29958b6cd840658aa279dc1db9b40fe78cd
Daniil Meshcheriakov <daniil.samsung.tab2@gmail.com>
2025-10-25 23:02:03 +0300

    feat: complete lab3

---
+++ students/k3340/Meshcheryakov_Daniil/Lr3/task_1/library_api/wsgi.py
@@ -0,0 +1,12 @@
+"""
+WSGI config for library_api project.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_api.settings')
+
+application = get_wsgi_application()
+
